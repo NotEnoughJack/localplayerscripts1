7d6fe755068554e430bb3e9a2f7ed8f74caa46b1
Jack <notjack137@gmail.com>
2025-06-07 01:44:17 -0500

    Update scriptv2.lua

--- scriptv2.lua
+++ scriptv2.lua
@@ -1,7 +1,7 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local Window = Fluent:CreateWindow({
-    Title = "Script Hub",
-    SubTitle = "Toggle Scripts Easily",
+    Title = "Kenma GUI",
+    SubTitle = "Version Two",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Theme = "Dark",
